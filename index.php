bfd1f11c9ebbf81dc63f9691dac78f8d2d1e76b2
Gabriela Menendez <gabriela.menendezfgksa@sv.cds>
2019-04-10 16:28:49 -0600

    pull de dany

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="./views/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
 <link rel="stylesheet" href="./views/css/style.css">
 
 </head>
@@ -12,11 +18,11 @@
 <div class="topnav" id="myTopnav">
   <a href="index.php" class="active">Inicio</a>
   
-  <div class="dropdown">
+  <div class="drop">
     <button class="dropbtn">Mantenimiento 
       <i class="fa fa-caret-down"></i>
     </button>
-    <div class="dropdown-content">
+    <div class="drop-content">
       <a href="views/index_sede.php">Sede</a>
       <a href="views/index.php">Curso</a>
       <a href="views/index.php">Cohorte</a>
@@ -35,7 +41,156 @@
 
 <div style="padding-left:16px">
 
-</div>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<link rel="stylesheet" href="views/css/menu.css">
+
+
+    <h1 class="text-center"> MENU DE COHORTES </h1>
+
+	
+	<div class="container">
+	<div class="row">
+	
+	<!--team-1-->
+	<div class="col-lg-4">
+	<div class="our-team-main">
+	
+	<div class="team-front ">
+	<img src="http://placehold.it/110x110/9c27b0/fff?text=1" class="img-fluid" />
+	<h3>HTML</h3>
+	<p>Cohorte 1</p>
+	</div>
+	
+	<div class="team-back ">
+	<span>
+    <br>
+	<a href="" class="btn-style">Registrar Notas</a><br><br>
+  <a href="" class="btn-style">Ver Notas</a>
+	</span>
+	</div>
+	
+	</div>
+	</div>
+	<!--team-1-->
+	
+	<!--team-2-->
+	<div class="col-lg-4">
+	<div class="our-team-main">
+	
+	<div class="team-front">
+	<img src="http://placehold.it/110x110/336699/fff?text=2" class="img-fluid" />
+  <h3>C# XAMARIN</h3>
+	<p>Cohorte 2</p>
+	</div>
+	
+	<div class="team-back">
+	<span>
+  <br>
+	<a href="" class="btn-style">Registrar Notas</a><br><br>
+  <a href="" class="btn-style">Ver Notas</a>
+	</span>
+	</div>
+	
+	</div>
+	</div>
+	<!--team-2-->
+	
+	<!--team-3-->
+	<div class="col-lg-4">
+	<div class="our-team-main">
+	
+	<div class="team-front">
+	<img src="http://placehold.it/110x110/607d8b/fff?text=3" class="img-fluid" />
+  <h3>HTML</h3>
+	<p>Cohorte 3</p>
+	</div>
+	
+	<div class="team-back">
+	<span>
+  <br>
+	<a href="" class="btn-style">Registrar Notas</a><br><br>
+  <a href="" class="btn-style">Ver Notas</a>
+	</span>
+	</div>
+	
+	</div>
+	</div>
+	<!--team-3-->
+	
+	<!--team-4-->
+	<div class="col-lg-4">
+	<div class="our-team-main">
+	
+	<div class="team-front">
+	<img src="http://placehold.it/110x110/4caf50/fff?text=4" class="img-fluid" />
+  <h3>PHP</h3>
+	<p>Cohorte 4</p>
+	</div>
+	
+	<div class="team-back">
+	<span>
+  <br>
+	<a href="" class="btn-style">Registrar Notas</a><br><br>
+  <a href="" class="btn-style">Ver Notas</a>
+	</span>
+	</div>
+	
+	</div>
+	</div>
+	<!--team-4-->
+	
+	<!--team-5-->
+	<div class="col-lg-4">
+	<div class="our-team-main">
+	
+	<div class="team-front">
+	<img src="http://placehold.it/110x110/e91e63/fff?text=5" class="img-fluid" />
+  <h3>XAMARIN</h3>
+	<p>Cohorte 5</p>
+	</div>
+	
+	<div class="team-back">
+	<span>
+  <br>
+	<a href="" class="btn-style">Registrar Notas</a><br><br>
+  <a href="" class="btn-style">Ver Notas</a>
+	</span>
+	</div>
+	
+	</div>
+	</div>
+	<!--team-5-->
+	
+	<!--team-6-->
+	<div class="col-lg-4">
+	<div class="our-team-main">
+	
+	<div class="team-front">
+	<img src="http://placehold.it/110x110/2196f3/fff?text=6" class="img-fluid" />
+	<h3>PHP</h3>
+	<p>Cohorte 6</p>
+	</div>
+	
+	<div class="team-back">
+	<span>
+  <br>
+	<a href="" class="btn-style">Registrar Notas</a><br><br>
+  <a href="" class="btn-style">Ver Notas</a>
+	</span>
+	</div>
+	
+	</div>
+	</div>
+	<!--team-6-->
+	
+	
+	
+	</div>
+	</div>
+    
 
 <script>
 function myFunction() {
